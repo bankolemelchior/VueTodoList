1a0a258bf7063bb3c5a670e689e12c70c2585be2
Omotosho <bankolemelchior@gmail.com>
2024-06-13 19:34:39 +0200

    tâche rendu dynamique

--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -3,32 +3,20 @@
     <TodoHeader/>
 
     <!-- liste des tâches -->
-    <main>
+    <main class="main">
+      <div class="">
+        <input type="checkbox" id="toggle-all-input" class="">
+        <label for="toggle-all-input" class="toggle-all-label">Tout afficher</label>
+      </div>
+
       <ul class="todo-list">
-        <li>
+        <li v-for="todo in todos" :key="todo.id">
         <div class="view">
-          <input type="checkbox" name="" id="" class="toggle">
-          <label for="">Tâche 1</label>
+          <input type="checkbox" class="toggle">
+          <label :for="todo.id">{{ todo.title }}</label>
         </div>
         </li>
-        <li>
-        <div class="view">
-          <input type="checkbox" name="" id="" class="toggle">
-          <label for="">Tâche 1</label>
-        </div>
-        </li>
-        <li>
-        <div class="view">
-          <input type="checkbox" name="" id="" class="toggle">
-          <label for="">Tâche 2</label>
-        </div>
-        </li>
-        <li>
-        <div class="view">
-          <input type="checkbox" name="" id="" class="toggle">
-          <label for="">Tâche 3</label>
-        </div>
-        </li>
+
       </ul>
     </main>
 
@@ -37,8 +25,16 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import TodoFooter from "../components/TodoFooter.vue";
 import TodoHeader from "../components/TodoHeader.vue";
+const count = 1;
+const todos = ref([
+  {id:'15fc4791-46cf-449b-beaf-ba7439e2fd31', title:'Ma tâche ', completed:false, started:false},
+  {id:'4e8f02b7-3d21-46c9-982e-caa963a6e961', title:'Ma tâche ', completed:false, started:false},
+  {id:'350e2d74-16c9-4911-8f26-f161b7deab66', title:'Ma tâche ', completed:false, started:false},
+  {id:'676fb886-fdcf-4430-96f4-9aac47434a66', title:'Ma tâche ', completed:false, started:false}
+]);
 </script>
 
 <style scoped></style>
